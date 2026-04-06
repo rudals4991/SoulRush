0bcf4a61f127e698b1bd9fd1f3e844bc5432b733
ParkGyoungMin <rudals20102@gmail.com>
2026-04-06 22:11:07 +0900

    feat: CharacterBase / IDamageable 인터페이스 / CharacterStat SO 설계

---
+++ Assets/Scripts/SO/CharacterStat.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Game/CharacterStat")]
+public class CharacterStat : ScriptableObject
+{
+    [Header("캐릭터 체력")]
+    public float baseMaxHp;
+
+    [Header("캐릭터 이동속도")]
+    public float baseMoveSpeed;
+
+    [Header("캐릭터 스테미나")]
+    public float baseMaxStamina;
+    public float staminaRegen;
+
+    [Header("캐릭터 데미지")]
+    public float attackDamage;
+}
