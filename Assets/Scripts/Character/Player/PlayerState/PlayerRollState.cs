de15e3a5eadec3565f15ee8c4af7121e7f212392
ParkGyoungMin <rudals20102@gmail.com>
2026-04-07 23:20:14 +0900

    feat: Player FSM 뼈대 구성

---
+++ Assets/Scripts/Character/Player/PlayerState/PlayerRollState.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class PlayerRollState : PlayerState
+{
+    public override PlayerStateType StateType => PlayerStateType.Roll;
+    public PlayerRollState(Player player, PlayerStateMachine stateMachine) : base(player, stateMachine)
+    {
+    }
+    public override void Enter()
+    {
+
+    }
+    public override void Update()
+    {
+
+    }
+    public override void Exit()
+    {
+
+    }
+}
